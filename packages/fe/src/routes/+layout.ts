5356f9516e405c84d9cb181d4995a78b4183187f
nicodemoraffaele <raffaele@efebia.com>
2025-11-22 21:51:08 +0100

    remove duplicate layout.js

--- packages/fe/src/routes/+layout.ts
+++ packages/fe/src/routes/+layout.ts
@@ -2,6 +2,9 @@ import { loadLocaleAsync } from '$lib/i18n/i18n-util.async';
 
 import type { LayoutLoad } from './$types';
 
+export const prerender = true;
+export const ssr = false;
+
 export const load = (async (event) => {
 	const { locale } = event.data;
 	await loadLocaleAsync(locale);
